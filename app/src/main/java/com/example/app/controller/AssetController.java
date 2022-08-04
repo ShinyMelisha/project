c8bcc1f694a50bf0ffebb468563155db531d8137
ShinyMelisha <95032523+ShinyMelisha@users.noreply.github.com>
2022-08-04 15:18:13 +0530

    added validations

--- app/src/main/java/com/example/app/controller/AssetController.java
+++ app/src/main/java/com/example/app/controller/AssetController.java
@@ -3,11 +3,13 @@ package com.example.app.controller;
 import com.example.app.model.Asset;
 import com.example.app.repository.AssetRepository;
 import com.example.app.service.AssetService;
+import com.example.app.utils.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.annotation.AccessType;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/asset")
@@ -29,27 +31,34 @@ public class AssetController {
         return "This is view page";
     }
 
-    @GetMapping("/all")
+    @GetMapping("/getAll")
     public List<Asset> getAll(){
         List<Asset> assets = this.assetRepository.findAll();
         return assets;
     }
 
-    @ PostMapping("/add")
-    public String addAsset(@RequestBody Asset asset){
+    @PostMapping("/add")
+    public String addAsset(@Valid @RequestBody Asset asset) throws ResourceNotFoundException{
         assetService.addAsset(asset);
         return "Successfully added";
     }
-    @PostMapping("/edit")
-    public Asset editAsset(@RequestBody Asset asset){
 
+    @PostMapping("/edit")
+    public Asset editAsset( @Valid @RequestBody Asset asset){
         return assetService.editAsset(asset);
     }
 
-    @DeleteMapping("/delete/{id}")
-    public String deleteByAssetId(@PathVariable("id") String id){
-        assetService.deleteByAssetId(id);
-        return "Successfully deleted";
+    @GetMapping("/get/{assetId}")
+    public Optional<Asset> getAsset(@PathVariable("assetId") String id) throws ResourceNotFoundException{
+        return assetService.getAssetById(id);
     }
+
+    @DeleteMapping("/delete/{id}")
+    public String deleteByAssetId(@PathVariable("id") String id) {
+        String result =assetService.deleteByAssetId(id);
+        return result;
+    }
+
+
 
 }
--- app/src/main/java/com/example/app/repository/AssignmentRepository.java
+++ app/src/main/java/com/example/app/repository/AssignmentRepository.java
@@ -1,9 +1,13 @@
 package com.example.app.repository;
 
+import com.example.app.model.Asset;
 import com.example.app.model.Assignment;
 import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Optional;
 
 public interface AssignmentRepository extends MongoRepository<Assignment,String> {
     Assignment save(Assignment assignment);
     void deleteById(String assignmentId);
+    Optional<Assignment> findById(String assignmentId);
 }
